d1db68f3d5797482b3a4f9e367d2fe0430fd5488
Joyee <veraqxf@gmail.com>
2024-06-07 18:10:23 +0800

    feat: Teleport

--- app.vue
+++ app.vue
@@ -3,5 +3,25 @@
 </script>
 
 <template>
-  <div></div>
+  <NuxtExample dir="advanced/teleport">
+    <div>
+      <Teleport to="body">
+        <UContainer>
+          <UAlert class="mt-8" icon="i-heroicons-server-stack" title="SSR Teleport"
+            description="Hello from Server-side teleport!"></UAlert>
+        </UContainer>
+      </Teleport>
+
+      <ClientOnly>
+        <Teleport to="body">
+          <UContainer>
+            <UAlert class="mt-8" icon="i-heroicons-window" title="Client Teleport"
+              description="Hello from Client-side teleport!" />
+          </UContainer>
+        </Teleport>
+      </ClientOnly>
+
+      <MyModal title="Teleport Modal">Hello from Modal teleport!</MyModal>
+    </div>
+  </NuxtExample>
 </template>
