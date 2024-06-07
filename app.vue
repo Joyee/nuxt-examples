0590ff482d440b8ac4a186e030f6943fffdfb02c
Joyee <veraqxf@gmail.com>
2024-06-07 16:03:02 +0800

    feat: locale

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
-
+const locale = useLocale()
+const locales = useLocales()
+const date = useLocaleDate(new Date('2024-06-07'))
 </script>
 
 <template>
-  <div></div>
+  <NuxtExample dir="advanced/locale">
+    <div class="flex justify-center gap-8 items-center mt-2">
+      <USelect v-model="locale" :options="locales" icon="i-heroicons-calendar-days" />
+      <p class="text-4xl">{{ date }}</p>
+    </div>
+  </NuxtExample>
 </template>
